ec86c7022718aa6c0935afd3e52c8c767046ad35
Monzon <Monzon@JMs-MacBook-Air.hsd1.ca.comcast.net>
2020-07-11 13:17:17 -0700

    Message

--- intro/src/pages/Index.vue
+++ intro/src/pages/Index.vue
@@ -1,14 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="" align="center">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
     >
+    <div>
+      <button v-on:click="testAlert()"  type="button">Text</button>
+      <button v-on:click="testAlert()" type="button">Text</button>
+      <input type="text" v-model="name2">
+    </div>
+    <div align="center">
+      {{ name }}
+      <q-btn v-on:click="testAlert()">Text</q-btn>
+      <q-input outlined v-model="name" style="width: 150px;"/>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      name: null,
+      name2: null
+    }
+  },
+  methods: {
+    testAlert: function () {
+      alert('Hello, world!' + this.name + ' ' + this.name2)
+    }
+  }
 }
 </script>
